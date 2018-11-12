b587f169eb1d1d795871f7e7acb64be0d5c2a264
Kaiyu Song <kaiyusong1994@gmail.com>
2018-11-11 18:52:48 -0800

    Update
    frinish one page build start build table view

--- SampleUnitTranslate/SampleUnitTranslate/LengthViewController.swift
+++ SampleUnitTranslate/SampleUnitTranslate/LengthViewController.swift
@@ -2,29 +2,81 @@ import UIKit
 
 class LengthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    let colors = ["Red","Yellow","Green","Blue","A","B","C","D"]
+    let lengthunit = ["m","km","dm","cm","mm","µm","nm","in","mi","ft","yd"]
+    let lengthkey = [1.0,0.001,10.0,100.0,1000.0,1000000.0,1000000000.0,39.3700787,0.0006213711922373,3.280839895013,1.093613298338]
+    
+    var fromindex = 0
+    var toindex = 0
+    var fromvalue = 0.0
+    var tovalue = 0.0
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return colors.count
+        return lengthunit.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return colors[row]
+        return lengthunit[row]
     }
     
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == frompick {
+            fromindex = row
+        }
+        if pickerView == topick {
+            toindex = row
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        testpicker.delegate = self
-        testpicker.dataSource = self
+        frompick.delegate = self
+        frompick.dataSource = self
+        topick.delegate = self
+        topick.dataSource = self
     }
     
-    @IBOutlet weak var testpicker: UIPickerView!
+    @IBOutlet weak var frompick: UIPickerView!
+    @IBOutlet weak var topick: UIPickerView!
+    @IBOutlet weak var inputvalue: UITextField!
+    @IBOutlet weak var outputvalue: UILabel!
     
+    @IBAction func calculate(_ sender: Any) {
+        if let input = Double(inputvalue.text!) {
+            tovalue = (input/lengthkey[fromindex])*lengthkey[toindex]
+            outputvalue.text = String(tovalue)
+        } else {
+            print("Not a valid number: \(inputvalue.text!)")
+        }
+    }
+    
+    @IBAction func goback(_ sender: Any) {
+        performSegue(withIdentifier: "goback", sender: self)
+    }
+    
+    
+    @IBAction func gotableview(_ sender: Any) {
+        performSegue(withIdentifier: "gototableview", sender: self)
+    }
+    
+    @IBAction func SaveData(_ sender: Any) {
+        var items : [String]
+        let SavedLength = UserDefaults.standard.object(forKey: "length")
+        if let temp = SavedLength as? [String] {
+            items = temp
+            items.append(inputvalue.text! + "  " + String(lengthunit[fromindex]) + "  " + outputvalue.text! + "  " + String(lengthunit[toindex]))
+            
+        }else{
+            items = [inputvalue.text! + "  " + String(lengthunit[fromindex]) + "  " + outputvalue.text! + "  " + String(lengthunit[toindex])]
+        }
+        print(items)
+        
+        UserDefaults.standard.set(items, forKey: "length")
+        
+    }
     
     
 }
---
+++ SampleUnitTranslate/SampleUnitTranslate/TableViewController.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "text"
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
